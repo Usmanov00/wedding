cf1aaa9d3a800902b3445ae2bd7c8b7f4f603f77
Usmanov <roma.maksato@gmail.com>
2022-09-27 21:23:19 +0600

    new

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           <div className="name">
             <div className="name1">Адил</div>
             <div className="name2">&</div>
-            <div className="name3">Багира</div>
+            <div className="name3">Махира</div>
           </div>
 
           <div className="time">
@@ -40,13 +40,45 @@ function App() {
               <p className="dress_text">Мы будем рады видеть девушек в <br/> вечерних платьях, а мужчин
                 в <br/> классических костюмах.</p>
                 <br/>
-
             </div>
           </div>
-
         </div>
       </div>
+      <div className="timing">
+        <p className="schedule">Свадебное расписание</p>
+        <div className="timing_time">
+          <span>15:00</span>
+          <p className="timing_time_txt">Сбор Гостей. Начало <br/>  фуршета</p>
+        </div>
+        <div className="timing_time">
+          <span>16:00</span>
+          <p className="timing_time_txt">Торжественная <br/> церемония</p>
+        </div>
+        <div className="timing_time">
+          <span>17:00</span>
+          <p className="timing_time_txt">Начало банкета</p>
+        </div>
+        <div className="timing_time">
+          <span>22:00</span>
+          <p className="timing_time_txt">Завершение  <br/>официалной части. <br/> Празднечный салют</p>
+        </div>
+        <div className="timing_time">
+          <span>23:00</span>
+          <p className="timing_time_txt">окончания банкета</p>
+        </div>
+       <div className="map">
+         {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+         <iframe
+           src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2926.4532592350456!2d74.6235815!3d42.8210385!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2skg!4v1664291625662!5m2!1sen!2skg"
+           width="600" height="450" allowFullScreen="" loading="lazy"
+           referrerPolicy="no-referrer-when-downgrade">
+
+         </iframe>
+       </div>
+      </div>
+      <p className="copyright">USMANOV</p>
     </div>
+
 
   );
 }
